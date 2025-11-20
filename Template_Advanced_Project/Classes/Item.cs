2805ec91d8419b4ed424b20a0a5282b0812f7855
Fratched <domenicobarille@gmail.com>
2025-11-20 18:09:06 -0500

    Question1-Completed

---
+++ Template_Advanced_Project/Classes/Item.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Template_Advanced_Project.Classes
+{
+    public class Item
+    {
+        private string name;
+        private string price;
+
+
+        public string Name { get => name; set => name = value; }
+        public string Price { get => price; set => price = value; }
+
+
+        public Item(string name, string price)
+        {
+            this.name = name;
+            this.price = price;
+        }
+
+        public override string ToString()
+        {
+
+            return "Item: " + Name + ", Price: " + Price;
+
+
+        }
+
+     
+    }
+}
